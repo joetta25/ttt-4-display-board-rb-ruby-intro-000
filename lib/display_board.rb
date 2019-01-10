81e83a876ecbeacb669c5cd113bb9ba88772ad01
joetta Hall <joetta.chasidy@gmail.com>
2019-01-10 20:04:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,13 +9,4 @@ def display_board(board)
 end
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-display_board(board[4])
+def display_board(board[4])
